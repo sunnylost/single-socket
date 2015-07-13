504f394e2cc312a1a83ad2801a20b3099f6639ba
sunnylost <sunnylost@gmail.com>
2015-07-13 17:24:43 +0800

    ok, that's it.

--- src/singleSocket.js
+++ src/singleSocket.js
@@ -6,38 +6,66 @@
  * 真实的连接获取到数据后，更新 localStorage
  * 其他标签页监听 storage 事件
  */
-let global    = window,
-LS            = localStorage,
 
-gid           = 1,
-isRealConnect = false,
-realSocketFns = [ '_handler', '_emitHandler', 'emit', 'on' ],
-connectInstance
+let global            = window,
+    LS                = localStorage,
+
+    gid               = 1,
+    isRealConnect     = false,
+    realSocketFns     = [ '_handler', '_emitHandler', 'emit', 'on' ],
+    $check            = Symbol( 'check' ),
+    $clean            = Symbol( 'clean' ),
+    $emitHandler      = Symbol( 'emitHandler' ),
+    $events           = Symbol( 'events' ),
+    $fn               = Symbol( 'fn' ),
+    $handler          = Symbol( 'handler' ),
+    $id               = Symbol( 'ID' ),
+    $isReal           = Symbol( 'isReal' ),
+    $recommend        = Symbol( 'recommend' ),
+    $timeoutID        = Symbol( 'timeoutID' ),
+    $upgrade          = Symbol( 'upgrade' ),
+    fakeSocketMethods = [ $clean, $isReal, $handler, $emitHandler, 'on', 'emit' ],
+    connectInstance, sinceAfter
 
 const BLANK                = '',
-      CHECK_GAP            = 5000,
-      BEFORE_UNLOAD        = 'unload',
+      UPDATE_RATE          = 5000,
+      CHECK_RATE           = 2 * UPDATE_RATE,
+      MAX_RESET_TIME       = 1000 * 60 * 60 * 24,
+      UNLOAD               = 'unload',
       EVENT_NAME           = 'storage',
       FUNCTION             = 'function',
+      UNDEFINED            = 'undefined',
       PRE                  = 'single-socket',
       GID                  = `${PRE}-gid`,
       ACTIVE_ID            = `${PRE}-active-id`,
       METHOD               = `${PRE}-method`,
+      RECOMMEND_ID         = `${PRE}-recommend-id`,
       UPDATE               = `${PRE}-update`,
       HAS_REAL_CONNECT     = `${PRE}-has-real-connect`,
       HAS_UPDATE           = `${PRE}-has-update`,
       LAST_ACTIVE_TIME     = `${PRE}-last-active-time`,
       TRIGGER_EVENT_PARAMS = `${PRE}-trigger-event-params`
 
+sinceAfter = checkTime => {
+    let lastActiveTime = LS.getItem( LAST_ACTIVE_TIME )
+
+    if ( lastActiveTime ) {
+        lastActiveTime = new Date( +lastActiveTime )
+        return Date.now() - lastActiveTime >= checkTime
+    } else {
+        return true
+    }
+}
+
 class RealSocket {
     constructor() {
         console.log( 'real' )
         this.socket   = io.connect()
         isRealConnect = true
-        global.addEventListener( EVENT_NAME, this._fn = this::this._emitHandler )
+        global.addEventListener( EVENT_NAME, this[ $fn ] = this::this[ $emitHandler ] )
     }
 
-    _handler( name, data, fn ) {
+    [ $handler ]( name, data, fn ) {
         typeof fn === FUNCTION && fn.call( null, data )
         LS.setItem( UPDATE, JSON.stringify( data ) )
         LS.setItem( METHOD, name )
@@ -46,7 +74,7 @@ class RealSocket {
         LS.setItem( HAS_UPDATE, '1' )
     }
 
-    _emitHandler( e ) {
+    [ $emitHandler ]( e ) {
         let key      = e.key,
             newValue = e.newValue
 
@@ -56,42 +84,51 @@ class RealSocket {
     }
 
     on( name, fn ) {
-        this.socket.on( name, data => this._handler( name, data, fn ) )
+        this.socket.on( name, data => this[ $handler ]( name, data, fn ) )
     }
 
     emit( ...args ) {
         this.socket.emit.apply( this.socket, args )
     }
 
-    clean() {
+    [ $clean ]() {
         this.socket.disconnect()
-        global.removeEventListener( EVENT_NAME, this._fn )
+        global.removeEventListener( EVENT_NAME, this[ $fn ] )
     }
 
-    isReal() {
-        return this.id == LS.getItem( ACTIVE_ID )
+    [ $isReal ]() {
+        return this[ $id ] == LS.getItem( ACTIVE_ID )
     }
 }
 
 class FakeSocket {
     constructor() {
         console.log( 'fake' )
-        this._events = {}
-        global.addEventListener( EVENT_NAME, this._fn = this::this._handler )
+        this[ $events ] = {}
+        this[ $check ]()
+        global.addEventListener( EVENT_NAME, this[ $fn ] = this::this[ $handler ] )
     }
 
-    _handler( e ) {
+    [ $check ]() {
+        this[ $timeoutID ] = setTimeout( () => {
+            if ( sinceAfter( CHECK_RATE ) ) {
+                this[ $recommend ]()
+            } else {
+                this[ $check ]()
+            }
+        }, CHECK_RATE )
+    }
+
+    [ $handler ]( e ) {
         let key = e.key,
-            method,
-            params,
-            data
+            method, data, rawData
 
         if ( key === HAS_UPDATE && e.newValue !== BLANK ) {
-            method = this._events[ LS.getItem( METHOD ) ]
-            params = JSON.parse( LS.getItem( TRIGGER_EVENT_PARAMS ) )
-            data   = JSON.parse( LS.getItem( UPDATE ) )
+            method  = this[ $events ][ LS.getItem( METHOD ) ]
+            rawData = LS.getItem( UPDATE )
+            data    = rawData != UNDEFINED && JSON.parse( rawData )
 
-            if ( params ) {
+            if ( data ) {
                 for ( var i = 0, len = method.length; i < len; i++ ) {
                     method[ i ]( data )
                 }
@@ -100,7 +137,7 @@ class FakeSocket {
     }
 
     on( name, fn ) {
-        let events = this._events
+        let events = this[ $events ]
 
         if ( !events[ name ] ) {
             events[ name ] = []
@@ -114,12 +151,44 @@ class FakeSocket {
         localStorage.setItem( TRIGGER_EVENT_PARAMS, JSON.stringify( args ) )
     }
 
-    clean() {
-        global.removeEventListener( EVENT_NAME, this._fn )
+    [ $clean ]() {
+        clearTimeout( this[ $timeoutID ] )
+        global.removeEventListener( EVENT_NAME, this[ $fn ] )
     }
 
-    isReal() {
+    [ $isReal ]() {
         return false
+    }
+
+    [ $recommend ] () {
+        if ( !LS.getItem( RECOMMEND_ID ) ) {
+            LS.setItem( RECOMMEND_ID, this[ $id ] )
+            setTimeout( this
+        ::
+            this[ $upgrade ], CHECK_RATE
+        )
+        } else {
+            this[ $check ]()
+        }
+    }
+
+    [ $upgrade ]() {
+        console.log( 'upgrade ', this[ $id ] )
+        this[ $clean ]()
+
+        let newConnectInstance = new RealSocket()
+        SingleSocket.update()
+        SingleSocket.connect( newConnectInstance )
+        LS.removeItem( RECOMMEND_ID )
+
+        fakeSocketMethods.forEach( method => {
+            connectInstance[ method ] = newConnectInstance::newConnectInstance[ method ]
+        })
+
+        for ( let key in connectInstance[ $events ] ) {
+            connectInstance[ $events ][ key ].forEach( fn => newConnectInstance.on( key, fn ) )
+
+        }
     }
 }
 
@@ -128,6 +197,7 @@ class SingleSocket {
         let hasConnect  = SingleSocket.hasConnect()
         connectInstance = new ( hasConnect ? FakeSocket : RealSocket )
         this.assign( connectInstance )
+        console.log( connectInstance[ $id ] )
 
         if ( isRealConnect ) {
             SingleSocket.update()
@@ -143,7 +213,13 @@ class SingleSocket {
 
     static connect( instance ) {
         LS.setItem( HAS_REAL_CONNECT, 1 )
-        LS.setItem( ACTIVE_ID, instance.id )
+        LS.setItem( ACTIVE_ID, instance[ $id ] )
+    }
+
+    static attempReset() {
+        if ( sinceAfter( MAX_RESET_TIME ) ) {
+            LS.setItem( GID, 0 )
+        }
     }
 
     /**
@@ -153,31 +229,39 @@ class SingleSocket {
         clearTimeout( SingleSocket._timeoutid )
         LS.setItem( LAST_ACTIVE_TIME, Date.now() )
 
-        SingleSocket._timeoutid = setTimeout( () => SingleSocket.update(), CHECK_GAP )
+        SingleSocket._timeoutid = setTimeout( () => SingleSocket.update(), UPDATE_RATE )
     }
 
     static disconnect() {
         if ( connectInstance ) {
-            connectInstance.isReal() && LS.removeItem( HAS_REAL_CONNECT )
-            connectInstance.clean()
+            connectInstance[ $isReal ]() && LS.removeItem( HAS_REAL_CONNECT )
+            connectInstance[ $clean ]()
         }
     }
 
     assign( instance ) {
-        let _gid = LS.getItem( GID )
+        SingleSocket.attempReset()
 
-        if ( !_gid ) {
-            _gid = 1
+        let _gid  = LS.getItem( GID ),
+            oldId = instance[ $id ]
+
+        if ( !oldId ) {
+            if ( !_gid ) {
+                _gid = 1
+            } else {
+                ++_gid
+            }
+
+            instance[ $id ] = _gid
         } else {
-            ++_gid
+            _gid = oldId
         }
 
-        instance.id = _gid
         LS.setItem( GID, _gid )
     }
 }
 
-window.addEventListener( BEFORE_UNLOAD, () => {
+window.addEventListener( UNLOAD, () => {
     SingleSocket.disconnect()
 } )
 
